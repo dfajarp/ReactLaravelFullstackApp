3472ceb974d285b3160b9b2063b2a7dd700e6d6d
fajar <dikfajar06@gmail.com>
2023-05-12 09:05:44 +0700

    React and Laravel Application

---
+++ react/src/views/UserForm.jsx
@@ -0,0 +1,129 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
+
+export default function UserForm() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState(null);
+    const { setNotification } = useStateContext();
+
+    const [user, setUser] = useState({
+        id: null,
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+    });
+
+    if (id) {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useEffect(() => {
+            setLoading(true);
+            axiosClient
+                .get(`/users/${id}`)
+                .then(({ data }) => {
+                    setLoading(false);
+                    setUser(data);
+                })
+                .catch(() => {
+                    setLoading(false);
+                });
+        }, []);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (user.id) {
+            axiosClient
+                .put(`/users/${user.id}`, user)
+                .then(() => {
+                    setNotification("User was successfully updated.");
+                    navigate("/users");
+                })
+                .catch((err) => {
+                    const res = err.response;
+
+                    if (res && res.status == 422) {
+                        setErrors(res.data.errors);
+                        console.log();
+                    }
+                });
+        } else {
+            axiosClient
+                .post("/users", user)
+                .then(() => {
+                    setNotification("User was successfully created.");
+
+                    navigate("/users");
+                })
+                .catch((err) => {
+                    const res = err.response;
+
+                    if (res && res.status == 422) {
+                        setErrors(res.data.errors);
+                        console.log();
+                    }
+                });
+        }
+    };
+
+    return (
+        <>
+            {user.id && <h1>Update User : {user.name}</h1>}
+            {!user.id && <h1>New User</h1>}
+            <div className="card animated fadeInDown">
+                {loading && <div className="text-center">Loading ...</div>}
+                {errors && (
+                    <div className="alert">
+                        {Object.keys(errors).map((key) => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                )}
+
+                {!loading && (
+                    <form onSubmit={onSubmit}>
+                        <input
+                            value={user.name}
+                            onChange={(e) =>
+                                setUser({ ...user, name: e.target.value })
+                            }
+                            type="text"
+                            placeholder="Name"
+                        />
+                        <input
+                            value={user.email}
+                            onChange={(e) =>
+                                setUser({ ...user, email: e.target.value })
+                            }
+                            type="email"
+                            placeholder="Email"
+                        />
+                        <input
+                            onChange={(e) =>
+                                setUser({ ...user, password: e.target.value })
+                            }
+                            type="password"
+                            placeholder="Passsowrd"
+                        />
+                        <input
+                            onChange={(e) =>
+                                setUser({
+                                    ...user,
+                                    password_confirmation: e.target.value,
+                                })
+                            }
+                            type="password"
+                            placeholder="Password Confirmation"
+                        />
+                        <button className="btn">Save</button>
+                    </form>
+                )}
+            </div>
+        </>
+    );
+}
